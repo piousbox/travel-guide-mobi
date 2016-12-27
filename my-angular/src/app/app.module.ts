8d7868bb4081fe3afd1cf816d507e42fd4d7baea
Victor Piousbox <piousbox@gmail.com>
2016-12-27 11:37:39 -0800

    bootstrap vendor, and some routes

--- my-angular/src/app/app.module.ts
+++ my-angular/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
@@ -15,26 +16,47 @@ import { EventsShowComponent } from './events-show/events-show.component';
 import { ManagerDashboardComponent } from './manager-dashboard/manager-dashboard.component';
 import { ManagerEventsEditComponent } from './manager-events-edit/manager-events-edit.component';
 
+const appRoutes: Routes = [
+    { path: '', redirectTo: '/users/firsttime', pathMatch: 'full' },
+    { path: 'about', component: AboutComponent },
+    
+    { path: 'users/firsttime', component: UsersFirsttimeComponent },
+    { path: 'users/login', component: UsersLoginComponent },
+    
+    { path: 'cities/:city_name/from/:from/until/:until', component: CitiesShowComponent },
+    
+    { path: 'events/:id', component: EventsShowComponent },
+    
+    { path: 'profiles/edit', component: ProfilesEditComponent },
+    
+    { path: 'travel-plans', component: TravelPlansIndexComponent },
+    { path: 'travel-plans/edit', component: TravelPlansEditComponent },
+    
+    { path: 'manager', component: ManagerDashboardComponent },
+    { path: 'manager/events/:id/edit', component: ManagerEventsEditComponent }
+];
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    AboutComponent,
-    UsersFirsttimeComponent,
-    UsersLoginComponent,
-    ProfilesEditComponent,
-    TravelPlansEditComponent,
-    CitiesShowComponent,
-    TravelPlansIndexComponent,
-    EventsShowComponent,
-    ManagerDashboardComponent,
-    ManagerEventsEditComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        AboutComponent,
+        UsersFirsttimeComponent,
+        UsersLoginComponent,
+        ProfilesEditComponent,
+        TravelPlansEditComponent,
+        CitiesShowComponent,
+        TravelPlansIndexComponent,
+        EventsShowComponent,
+        ManagerDashboardComponent,
+        ManagerEventsEditComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        RouterModule.forRoot(appRoutes)
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
