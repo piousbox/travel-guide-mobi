a4c1143ece7c6da8b35c15bc4ad3d08b4e0a975c
Victor Piousbox <piousbox@gmail.com>
2016-12-28 13:34:24 -0800

    setting up my-angular-webpack-2

---
+++ my-angular-webpack-2/README.md
@@ -0,0 +1,4 @@
+
+From: https://semaphoreci.com/community/tutorials/setting-up-angular-2-with-webpack
+
+ node ./node_modules/.bin/webpack --config webpack/webpack.dev.js
---
+++ my-angular-webpack-2/dist/app.bundle.js
@@ -0,0 +1,10 @@
+webpackJsonp([0],[
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	(function webpackMissingModule() { throw new Error("Cannot find module \"/home/piousbox/projects/travel-guide.mobi/my-angular-webpack-2/src/bootstrap\""); }());
+
+
+/***/ }
+]);
+//# sourceMappingURL=app.bundle.js.map
