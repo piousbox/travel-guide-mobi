efa0d1e1344928ec5864c99199f1e6f0a17d1daa
Victor Piousbox <piousbox@gmail.com>
2016-12-28 15:15:53 -0800

    its beginning to work maybe? im wiring jquery, framework7 and mobile.s in.

--- my-angular-webpack-3/src/main.browser.ts
+++ my-angular-webpack-3/src/main.browser.ts
@@ -3,6 +3,33 @@ import './polyfills.browser';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 
+import 'jquery';
+
+/**
+ * let's do it mobile.s
+ */
+import '../vendor/jquery/jquery-3.1.1.js';
+
+import '../vendor/framework7/css/framework7.ios.css';
+import '../vendor/mobile.s/css/swipebox.css';
+import '../vendor/mobile.s/css/animations.css';
+import '../vendor/mobile.s/css/style.css';
+
+import '../vendor/jquery/jquery.validate.min.js';
+import '../vendor/jquery/jquery.swipebox.js';
+import '../vendor/jquery/jquery.fitvids.js';
+import '../vendor/framework7/js/framework7.js';
+
+import '../vendor/mobile.s/js/email.js';
+import '../vendor/mobile.s/js/audio.min.js';
+import '../vendor/mobile.s/js/classie.js';
+import '../vendor/mobile.s/js/selectFx.js';
+// import '../vendor/mobile.s/js/my-app.js';
+
+/**
+ * before here, mobile.s
+ */
+
 export const platformRef = platformBrowserDynamic();
 
 export function main() {
