08f3d6e378bd2e2a065bd4122f965beefde4d4ce
Victor Piousbox <piousbox@gmail.com>
2016-12-29 12:34:03 -0800

    tests are almost beginning to be meaningful

--- my-react-1/test/HelloWorld-test.js
+++ my-react-1/test/HelloWorld-test.js
@@ -2,15 +2,21 @@ import React from 'react';
 import TestUtils from 'react-addons-test-utils';
 import NavComponent from '../src/components/nav.js';
 import { expect } from 'chai';
+import Sidebar from 'react-sidebar';
 
 let shallowRenderer = TestUtils.createRenderer();
 
 describe('NavComponent', () => {
-  // shallowRenderer.render(<NavComponent/>);
-  // let HelloWorldComponent = shallowRenderer.getRenderOutput();
-  // let HelloWorldChildren = HelloWorldComponent.props.children
+  shallowRenderer.render(<NavComponent/>);
+  let thisComponent = shallowRenderer.getRenderOutput();
+  let theseChildren = thisComponent.props.children;
 
   it('sanity', () => {
     expect( true ).to.equal( true );
   });
+
+  it('should render a div', () => {
+    expect( thisComponent.type ).to.equal( Sidebar );
+  });
+
 });
