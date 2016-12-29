b2547573c9c97103564d98b869c7b0324855c36b
Victor Piousbox <piousbox@gmail.com>
2016-12-29 14:58:15 -0800

    working on router

--- my-react-1/index.js
+++ my-react-1/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {render} from 'react-dom';
-import {Router, Route, hashHistory} from 'react-router';
+import { render } from 'react-dom';
+import { Router, Route, hashHistory } from 'react-router';
 import App from './src/app';
 
 // Global Styles
@@ -10,12 +10,15 @@ import './assets/styles/index.less';
 // Components
 import Page1 from './src/components/page1.js';
 import Page2 from './src/components/page2.js';
+import About from './src/components/about';
+import UsersFirsttime from './src/components/users-firsttime';
 
 render((
   <Router history={hashHistory}>
-    <Route path="/" component={App}>
-      <Route path="/page1" component={Page1}/>
-      <Route path="/page2" component={Page2}/>
-    </Route>
+    <Route path="/" component={App}/>
+    <Route path="/page1" component={Page1}/>
+    <Route path="/page2" component={Page2}/>
+    <Route path="/pages/about" component={About} />
+    <Route path="/users/firsttime" component={UsersFirsttime} />
   </Router>
 ), document.getElementById('app'));
--- my-react-1/src/components/nav.js
+++ my-react-1/src/components/nav.js
@@ -72,8 +72,8 @@ export default React.createClass({
     const contentHeader = (
       <span>
         {!this.state.docked &&
-         <a onClick={this.menuButtonClick} href="#" style={styles.contentHeaderMenuLink}>=</a>}
-        <span>React Sidebar</span>
+         <a onClick={this.menuButtonClick} href="javascript:void(0);" style={styles.contentHeaderMenuLink}>=</a>}
+        <span>This title</span>
       </span>
     );
 
@@ -94,22 +94,13 @@ export default React.createClass({
     return (
       <Sidebar {...sidebarProps}>
         <MaterialTitlePanel title={contentHeader}>
-          <div style={styles.content}>
-            <p>React Sidebar is a sidebar component for React. It offers the following features:</p>
-            <ul>
-              <li>Have the sidebar slide over main content</li>
-              <li>Dock the sidebar next to the content</li>
-              <li>Touch enabled: swipe to open and close the sidebar</li>
-              <li>Easy to combine with media queries for auto-docking (<a href="responsive_example.html">see example</a>)</li>
-              <li>Sidebar and content passed in as PORCs (Plain Old React Components)</li>
-              <li><a href="https://github.com/balloob/react-sidebar">Source on GitHub</a> (MIT license)</li>
-              <li>Only dependency is React</li>
+          <ul>
+            <li><h2>Where are you traveling to?</h2></li>
+            <li>Country:</li>
+            <li>City:</li>
+            <li>From:</li>
+            <li>Until:</li>
             </ul>
-            <p><a href="https://github.com/balloob/react-sidebar#installation">Instructions how to get started.</a></p>
-            <p><b>Current rendered sidebar properties:</b></p>
-            {['open', 'docked', 'transitions', 'touch', 'shadow', 'pullRight'].map(this.renderPropCheckbox)}
-            {['touchHandleWidth', 'dragToggleDistance'].map(this.renderPropNumber)}
-          </div>
         </MaterialTitlePanel>
       </Sidebar>
     );
