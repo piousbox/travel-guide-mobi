a84e52ef6974c540ce7d907d45dddb871939fc96
Victor Piousbox <piousbox@gmail.com>
2016-12-29 11:08:09 -0800

    as it should be, lets do the sliding menu now.

--- my-react-1/src/components/nav.js
+++ my-react-1/src/components/nav.js
@@ -5,9 +5,12 @@ export default React.createClass({
   render() {
     return (
       <div className="nav nav-pills nav-stacked">
-        <li><Link to="/page1">Page 1</Link></li>
-        <li><Link to="/page2">Page 2</Link></li>
+        <ul>
+          <li><Link to="/">Beginning</Link></li>
+          <li><Link to="/page1">Page 1</Link></li>
+          <li><Link to="/page2">Page 2</Link></li>
+        </ul>
       </div>
-    )
+    );
   }
-})
+});
