2958f759e57189a75d21a62a7256d4b3724f4ff8
Victor Piousbox <piousbox@gmail.com>
2016-12-30 10:25:37 -0800

    assets, flags

--- my-react-1/src/components/travel-plans/index.js
+++ my-react-1/src/components/travel-plans/index.js
@@ -1,9 +1,41 @@
 import React from 'react';
 
+import { Grid, Row, Col } from 'react-bootstrap';
+
+class Flag extends React.Component {
+  render () {
+    let source = "/assets/images/flag_icons/png/" + this.props.countryCode + ".png";
+
+    return (
+<img src={source} alt='' />
+    )
+  }
+}
+
 export default React.createClass({
+  styles: {
+    center: { textAlign: 'center' }
+  },
+
   render () {
     return(
-<div>return travel plans</div>
+<div>
+  <h5 style={this.styles.center}>travel plans</h5>
+
+  <div style={this.styles.center}>
+    <Flag countryCode="co" />
+    <h4>Medellin</h4>
+  </div>
+
+  <hr />
+  <h4 style={this.styles.center}>2016-01-01</h4>
+  <Row>
+    <Col xs={12}></Col>
+  </Row>
+
+  <hr />
+
+</div>
     )
   }
 });
